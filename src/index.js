ab3650c5fb9f12274a95bda71e4a540e59e5a6b2
naby880718 <naby880718@hanmail.net>
2023-09-20 10:02:18 +0900

    s

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </Provider>
